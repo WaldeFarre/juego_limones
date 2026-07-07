2776d3dcef338e6dd2cc427c8e04fe91288b6bc2
WaldeFarre <waldemarfarreldl2020@gmail.com>
2026-07-07 14:09:31 -0500

    finv1 juego lemons

--- limones.js
+++ limones.js
@@ -12,8 +12,10 @@ let limonX=canvas.width/2;
 let limonY=5;
 let puntaje=0;
 let vidas=3
+let velocidadCaida=200;
 
 function iniciar(){
+    setInterval(bajarLimon,velocidadCaida);
     dibujarSuelo();
     dibujarPersonaje();
     dibujarLimon();
