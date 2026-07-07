ffe75f48a60b562a906885d05b557744cffce981
WaldeFarre <waldemarfarreldl2020@gmail.com>
2026-07-07 10:02:02 -0500

    apresurado

--- limones.js
+++ limones.js
@@ -7,6 +7,7 @@ const ANCHO_PERSONAJE=40;
 const ANCHO_LIMON=20;
 const ALTO_LIMON=20;
 let personajeX= canvas.width/2;
+let personajeY=canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE);
 let limonX=canvas.width/2;
 let limonY=5;
 
@@ -22,17 +23,19 @@ function dibujarSuelo(){
 
 function dibujarPersonaje(){
     ctx.fillStyle='yellow';
-    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,personajeY,ANCHO_PERSONAJE,ALTURA_PERSONAJE);
 }
 
 function moverIzquierda(){
     personajeX=personajeX-10;
     actualizarPantalla();
+    detectarColision();
 }
 
 function moverDerecha(){
     personajeX=personajeX+10;
     actualizarPantalla();
+    detectarColision();
 }
 
 function actualizarPantalla(){
@@ -54,4 +57,10 @@ function dibujarLimon(){
 function bajarLimon(){
     limonY = limonY + 10;
     actualizarPantalla();
+}
+
+function detectarColision(){
+    if(limonX == personajeX && limonY == personajeY){
+        alert("ATRAPADO!!!!");
+    }
 }
